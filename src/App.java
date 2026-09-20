81acee56e31e951a88571076f5a4e857d727f981
Patrick Ryan Baber <ryan.baber@germantownacademy.org>
2026-09-20 13:15:49 -0500

    Add comments for clarity in main method
    Added comments to explain reader initialization and user input.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        // The line below creates a reader. It only needs to be done once, so just leave it at the top.
+        Reader read = new Reader();
+
+        // The line below creates a variable and uses read's nextInt function to get a number from the user.
+        int number = read.nextInt("Enter a number:");
+        System.out.println("You entered " + number);
+
+        // You can also use the Methods class I created to get random numbers.
     }
 }
