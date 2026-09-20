b3879acbc956335ce395e1fdc32efd87f6486c74
Patrick Ryan Baber <ryan.baber@germantownacademy.org>
2026-09-20 13:23:17 -0500

    Revise comments in App.java for better guidance
    Updated comments for clarity and instructions on math quiz implementation.

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // DO NOT EDIT OR DELETE LINES 4 TO 15. FEEL FREE TO COPY AND PAST AS NEEDED.
         // The line below creates a reader. It only needs to be done once, so just leave it at the top.
         Reader read = new Reader();
 
-        // The line below creates a variable and uses read's nextInt function to get a number from the user.
+        // The line below creates a variable and uses read's getInt function to get a number from the user.
         int number = read.getInt("Enter a number:");
         System.out.println("You entered " + number);
 
@@ -12,6 +12,19 @@ public class App {
         // Let's change the value of number, created above, to a random number from 1 to 100.
         number = Methods.randInt(1, 100);
         System.out.println("Your random number is " + number);
+
+        /* 
+         * Below this comment, write at least one math quiz question. 
+         * Your question should:
+         *     -generate two random values
+         *     -show the user a simple math quesiton using these values
+         *     -let the user enter an answer
+         *     -show the user the correct answer
+         *     -you do NOT need to evaluate whether the user is correct, but feel free to try!
+         */
+
+        // WRITE YOUR CODE BELOW THIS LINE:
+        
         
     }
 }
